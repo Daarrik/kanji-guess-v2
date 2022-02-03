ede4ae9fa66413f6424132eb4d390e3ac0f037a4
Daarrik <darrikwh@gmail.com>
2022-02-02 22:50:40 -0800

    type all the thingsgit add .!
    typescript is cool

--- src/constants/links.tsx
+++ src/constants/links.tsx
@@ -1,7 +1,13 @@
 import { ReactComponent as GithubIcon } from "../icons/github.svg";
 import { ReactComponent as LinkedinIcon } from "../icons/linkedin.svg";
 
-export const links = [
+interface LinkTypes {
+  name: string;
+  url: string;
+  icon: React.ReactElement;
+}
+
+export const links: Array<LinkTypes> = [
   {
     name: "github",
     url: "https://github.com/Daarrik",
