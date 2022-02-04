be47d5bb079ad4a865d71297f1e17045d5838757
Daarrik <darrikwh@gmail.com>
2022-02-04 03:23:24 -0800

    removed context
    will probably work to see how no context changes the project

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,14 +1,18 @@
-import { useContext } from "react";
-import { KanjiContext } from "./KanjiProvider";
-
 const Container = () => {
-  const { kanji, reading, newKanji } = useContext(KanjiContext);
+  const [{ kanji, reading }, setKanji] = useState({
+    kanji: "漢字",
+    reading: "かんじ",
+  });
+  const [guess, setGuess] = useState("");
+  const [guessed, setGuessed] = useState(false);
 
   return (
     <div>
       <h1 className="text-normal">{kanji}</h1>
       <p className="text-normal">{reading}</p>
-      <button onClick={newKanji}>fake kanji fetch button</button>
+      <button onClick={() => setKanji({ kanji: "試験", reading: "しけん" })}>
+        fake kanji fetch button
+      </button>
     </div>
   );
 };
