eb0010537d0a85d7bc104196af5e7aa4e7a25544
Daarrik <darrikwh@gmail.com>
2022-02-06 02:39:34 -0800

    removed false solution to charbox problem
    must sleep on this problem

--- src/components/input/box/BoxInput.tsx
+++ src/components/input/box/BoxInput.tsx
@@ -2,47 +2,19 @@ import React, { useState, useEffect, useContext } from "react";
 import { KanjiContext } from "../../context/KanjiProvider";
 import CharBox from "./CharBox";
 
-export interface BoxState {
-  idx: number;
-  box: React.ReactElement;
-  clicked: boolean;
-}
-
-interface CharBoxProps {
-  idx: number;
-  kana: string;
-}
-
 const BoxInput: React.FC = () => {
   const { reading } = useContext(KanjiContext);
-  const [boxes, setBoxes] = useState<BoxState[]>([]);
-
-  const clickBox = (boxClicked: number): void => {
-    setBoxes((prevBoxes) =>
-      prevBoxes.map(({ idx, box, clicked }) => ({
-        idx: idx,
-        box: box,
-        clicked: idx === boxClicked ? !clicked : clicked,
-      }))
-    );
-  };
+  const [boxes, setBoxes] = useState<React.ReactElement[]>([]);
 
   useEffect(() => {
     setBoxes(
-      reading.split("").map((kana, idx) => {
-        return {
-          idx: idx,
-          box: <CharBox key={idx} idx={idx} kana={kana} clickBox={clickBox} />,
-          clicked: false,
-        };
-      })
+      reading.split("").map((kana, idx) => <CharBox key={idx} kana={kana} />)
     );
   }, [reading]);
 
   return (
     <div>
-      <div>{boxes.map(({ box, clicked }) => !clicked && box)}</div>
-      <div>{boxes.map(({ box, clicked }) => clicked && box)}</div>
+      <div>{boxes}</div>
     </div>
   );
 };
--- src/components/input/box/CharBox.tsx
+++ src/components/input/box/CharBox.tsx
@@ -1,24 +1,12 @@
 import { useState, useContext, useEffect } from "react";
 import { GuessContext } from "../../context/GuessProvider";
-import { BoxState } from "./BoxInput";
 
 interface CharBoxProps {
-  idx: number;
   kana: string;
-  clickBox: (boxClicked: number) => void;
 }
 
-const CharBox: React.FC<CharBoxProps> = ({ idx, kana, clickBox }) => {
-  return (
-    <button
-      onClick={() => {
-        console.log(`From CharBox: ${idx}: ${kana}`);
-        clickBox(idx);
-      }}
-    >
-      {kana}
-    </button>
-  );
+const CharBox: React.FC<CharBoxProps> = ({ kana }) => {
+  return <button>{kana}</button>;
 };
 
 export default CharBox;
