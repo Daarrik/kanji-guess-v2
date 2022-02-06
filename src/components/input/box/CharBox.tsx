56c9ce135832f12a82a05e32025fe867b4354580
Daarrik <darrikwh@gmail.com>
2022-02-06 02:27:04 -0800

    commit before testing nested components

--- src/components/input/box/CharBox.tsx
+++ src/components/input/box/CharBox.tsx
@@ -1,4 +1,4 @@
-import { useState, useContext } from "react";
+import { useState, useContext, useEffect } from "react";
 import { GuessContext } from "../../context/GuessProvider";
 import { BoxState } from "./BoxInput";
 
