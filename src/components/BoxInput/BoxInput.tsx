a2efa6283144f8dba5b6521d7396a2883b1d0837
Darrik Houck <darrikwh@gmail.com>
2022-03-07 11:19:13 -0800

    new charbox progress

--- src/components/BoxInput/BoxInput.tsx
+++ src/components/BoxInput/BoxInput.tsx
@@ -3,15 +3,23 @@ import { KanjiContext } from "../ContextProviders/KanjiProvider";
 import { GuessContext } from "../ContextProviders/GuessProvider";
 import CharBox from "../CharBox/CharBox";
 
+interface CharBox {
+  id: number;
+  kana: string;
+  clicked: boolean;
+}
+
 const BoxInput: React.FC = () => {
   const { reading } = useContext(KanjiContext);
   const { guess } = useContext(GuessContext);
 
-  const [boxes, setBoxes] = useState<React.ReactElement[] | null>(null);
+  const [boxes, setBoxes] = useState<CharBox[] | null>(null);
 
   useEffect(() => {
     setBoxes(
-      reading.split("").map((kana, idx) => <CharBox key={idx} kana={kana} />)
+      reading
+        .split("")
+        .map((kana, idx) => ({ id: idx, kana: kana, clicked: false }))
     );
   }, [reading]);
 
